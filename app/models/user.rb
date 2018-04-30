5acf33242f2df97916d9ff50bba5ddd13f19d52d
chris_hopkins_2445 <cjhopkins1986@gmail.com>
2018-04-30 14:55:04 +0100

    added attachments to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/default_image.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
