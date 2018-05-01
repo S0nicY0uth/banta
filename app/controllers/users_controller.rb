4e0f22ad45c80de99df5495ed2fbb46e1f585863
chris_hopkins_2445 <cjhopkins1986@gmail.com>
2018-05-01 14:30:08 +0100

    styled basic chat look

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,11 @@ class UsersController < ApplicationController
     def index
     end
 
+    def home
+        @user = current_user
+        render :show, :layout => 'chat'
+    end
+
     def show
       @user = User.find(params[:id])
     end
