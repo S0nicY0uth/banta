e85132bd5ff62b2bb097d2ed2b64ce062e60b7b9
chris_hopkins_2445 <cjhopkins1986@gmail.com>
2018-05-22 13:56:21 +0100

    completed the JS front end

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,8 +6,12 @@ class Message < ApplicationRecord
     validates :user, presence: true
     validates :chat_room, presence: true
 
-    def push_message(current_user)
-        ActionCable.server.broadcast "MessagesChannel:#{chat_room.id}", message: to_html(current_user)
+    def push_message(current_user, format_type)
+        if format_type == 'html'
+            ActionCable.server.broadcast "MessagesChannel:#{chat_room.id}:html", message: to_html(current_user)
+        else          
+            ActionCable.server.broadcast "MessagesChannel:#{chat_room.id}", message: to_json
+        end
     end
 
     def to_html(current_user)
@@ -16,5 +20,9 @@ class Message < ApplicationRecord
         :locals => {message: self, current_user: current_user}
         )
     end
-    #ask ben about his to_html method
+
+    def to_json
+        JSON.generate({content: content, user: user.name, user_id: user.id, created_at: created_at})
+    end
+    
 end
